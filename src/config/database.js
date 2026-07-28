593e078fe5afb56c6459824d4148290a67daa713
sandeepvasaskr025-byte <sandeepvasaskr025@gmail.com>
2026-07-28 21:05:16 +0530

    db-atlas-connect

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const connectDB = async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/devtinder")
+    try{
+         await mongoose.connect("mongodb+srv://sandeepranisk25_db_user:tsGnyLVE0BoSfmre@cluster0.msjfl1k.mongodb.net/devtinder1?retryWrites=true&w=majority"
+        
+    )
+    console.log("Database connected")
+    }
+    catch(err){
+        console.error("mongodb connection error",err.message)
+    }
+   
 }
 
 module.exports = connectDB;
